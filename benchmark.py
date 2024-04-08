ebd40e14ce0d6aaddd5e36cd601ebc8f391b6672
Manoj Nath Yogi <manojnath112@gmail.com>
2024-04-08 02:15:51 -0400

    1st benchmark: 32-bit Integer operation benchmark - addition

--- benchmark.py
+++ benchmark.py
@@ -1,3 +1,11 @@
 import time
 import os
 import numpy as np
+
+def int_operations_benchmark():
+    print("Starting 32-bit integer operations...")
+    start_time = time.time()
+    # 1010 additions
+    a = 1
+    for _ in range(1010):
+        a += 1
